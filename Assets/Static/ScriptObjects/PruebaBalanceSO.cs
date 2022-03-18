df12efbcd0c30056bba0152150b16d751ef6e77f
José Abraham González Navarro <65511679+abrahamglez17@users.noreply.github.com>
2022-03-18 17:56:46 -0600

    creacion de scriptable object de prueba de balance

---
+++ Assets/Static/ScriptObjects/PruebaBalanceSO.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+[CreateAssetMenu(menuName = "Prueba Balance", fileName = "New Prueba Balance")]
+public class PruebaBalanceSO : ScriptableObject
+{
+    // variables serialized
+
+    [TextArea(2, 6)]
+    [SerializeField] string instrucciones = "Escribe las instrucciones aquí:";
+    [SerializeField] Image imagenInstruccion;
+    [SerializeField] string[] opciones = new string[3];
+
+    public string GetInstrucciones()
+    {
+        return instrucciones;
+    }
+
+}

52c2757cff79f888a4176f95cd994b09000cd24d
Monty3141 <juanpablomontoyae@gmail.com>
2022-03-17 19:27:03 -0700

    Se creo un prefab para cambiar de escena, esto contiene un script que se encarga de activarse con un boton

--- Assets/Scripts/UI/SceneManagerScript.cs
+++ Assets/Scripts/UI/SceneManagerScript.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class GenerateUI : MonoBehaviour
+using UnityEngine.SceneManagement;
+public class SceneManagerScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void loadScene(string scene)
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Single);
     }
 }
